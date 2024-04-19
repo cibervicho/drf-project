86110218ba7bc1cb90f8852b6b4fc6b7ecbcfe9b
David M. Moreno <david.mmoreno@gmail.com>
2024-04-19 10:36:44 -0600

    Extended the API to support POST, PUT and DELETE HTTP methods

--- watchmate/watchlist_app/models.py
+++ watchmate/watchlist_app/models.py
@@ -5,6 +5,6 @@ class Movie(models.Model):
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=200)
     active = models.BooleanField(default=True)
-    
+
     def __str__(self):
         return self.name
