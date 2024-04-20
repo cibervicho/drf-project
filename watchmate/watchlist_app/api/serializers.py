bdb9bb7992199bb6a9d3afc0baa0695c38c2950f
David M. Moreno <david.mmoreno@gmail.com>
2024-04-20 10:35:48 -0600

    Replaced the Serializer class with a ModelSerializer class

--- watchmate/watchlist_app/api/serializers.py
+++ watchmate/watchlist_app/api/serializers.py
@@ -2,38 +2,55 @@ from rest_framework import serializers
 from watchlist_app.models import Movie
 
 
-# Validator for the description
-def description_validator(value):
-    if len(value) < 5:
-        raise serializers.ValidationError("Movie description must be at least 5 characters long")
-    return value
+class MovieSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Movie
+        fields = "__all__"
+        #fields = ["id", "name", "description"]
+        #exclude = ["name"]
 
-class MovieSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField()
-    description = serializers.CharField(validators=[description_validator])
-    active = serializers.BooleanField()
-
-    def create(self, validated_data):
-        return Movie.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        instance.name = validated_data.get('name', instance.name)
-        instance.description = validated_data.get('description', instance.description)
-        instance.active = validated_data.get('active', instance.active)
-        instance.save()
-        return instance
-
-
-    # Level-field validation
-    def validate_name(self, value):
-        if len(value) < 2:
+    def validate_name(self, name):
+        if len(name) < 2:
             raise serializers.ValidationError("Movie name must be at least 2 characters long")
-        return value
+        return name
 
-    # Object-level validation
     def validate(self, data):
         if data["name"] == data["description"]:
             raise serializers.ValidationError("Movie name must be different to it's description")
         return data
+
+# # Validator for the description
+# def description_validator(value):
+#     if len(value) < 5:
+#         raise serializers.ValidationError("Movie description must be at least 5 characters long")
+#     return value
+
+# class MovieSerializer(serializers.Serializer):
+#     id = serializers.IntegerField(read_only=True)
+#     name = serializers.CharField()
+#     description = serializers.CharField(validators=[description_validator])
+#     active = serializers.BooleanField()
+
+#     def create(self, validated_data):
+#         return Movie.objects.create(**validated_data)
+
+#     def update(self, instance, validated_data):
+#         instance.name = validated_data.get('name', instance.name)
+#         instance.description = validated_data.get('description', instance.description)
+#         instance.active = validated_data.get('active', instance.active)
+#         instance.save()
+#         return instance
+
+
+#     # Level-field validation
+#     def validate_name(self, value):
+#         if len(value) < 2:
+#             raise serializers.ValidationError("Movie name must be at least 2 characters long")
+#         return value
+
+#     # Object-level validation
+#     def validate(self, data):
+#         if data["name"] == data["description"]:
+#             raise serializers.ValidationError("Movie name must be different to it's description")
+#         return data
 
