19e42bcbe94eeac95770faeb8e505814b4f8d6f1
David M. Moreno <david.mmoreno@gmail.com>
2024-04-27 13:42:57 -0600

    Added support to generic views and mixins to simplify the Create, Read and Update

--- watchmate/watchlist_app/api/urls.py
+++ watchmate/watchlist_app/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 # from watchlist_app.api.views import movie_list, movie_detail
-from watchlist_app.api.views import WatchListAV, WatchDetailAV, StreamPlatformAV, StreamPlatformDetailAV
+from watchlist_app.api.views import WatchListAV, WatchDetailAV, StreamPlatformAV, StreamPlatformDetailAV, ReviewList, ReviewDetail
 
 urlpatterns = [
     # Function Based Views
@@ -11,5 +11,8 @@ urlpatterns = [
     path('list/', WatchListAV.as_view(), name='watchlist-list'),
     path('<int:pk>/', WatchDetailAV.as_view(), name='watchlist-detail'),
     path('stream/', StreamPlatformAV.as_view(), name='stream-list'),
-    path('stream/<int:pk>/', StreamPlatformDetailAV.as_view(), name='stream-detail')
+    path('stream/<int:pk>/', StreamPlatformDetailAV.as_view(), name='stream-detail'),
+
+    path('review/', ReviewList.as_view(), name='review-list'),
+    path('review/<int:pk>/', ReviewDetail.as_view(), name='review-detail')
 ]
